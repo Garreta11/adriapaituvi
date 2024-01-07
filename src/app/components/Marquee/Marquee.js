c3e8ba0c773a8b9b24009a4ff1a0c1fe82915ef3
Jordi Garreta <jordigarreta11@gmail.com>
2024-01-07 11:20:06 +0100

    improve footer cities

--- src/app/components/Marquee/Marquee.js
+++ src/app/components/Marquee/Marquee.js
@@ -1,51 +1,37 @@
 'use client'
 
 import styles from './Marquee.module.scss'
-import { useEffect, useRef } from 'react';
-
 
 const Marquee = ({ cities }) => {
 
-    const marqueeRef = useRef()
-
-    useEffect(() => {
-        const marquee = marqueeRef.current;
-        let animation;
-
-        const resetAnimation = () => {
-            marquee.style.transform = 'translateX(100%)';
-            marquee.style.transition = 'none';
-            void marquee.offsetWidth; // Trigger reflow
-            marquee.style.transition = 'transform 10s linear infinite';
-        };
-
-        const handleAnimationIteration = () => {
-            resetAnimation();
-        };
-
-        marquee.addEventListener('animationiteration', handleAnimationIteration);
-
-        resetAnimation(); // Initial reset
-
-        return () => {
-            marquee.removeEventListener('animationiteration', handleAnimationIteration);
-            cancelAnimationFrame(animation);
-        };
-    }, []);
-
-
     return (
-        <div ref={marqueeRef} className={styles.marquee__cities}>
-            {cities.map((city, index) => {
-                return (
-                    <p
-                        className={`${styles.marquee__cities__city} ${city.current_city ? styles.marquee__cities__city__current : ''}`}
-                        key={index}
-                    >
-                        {city.city}
-                    </p>
-                )
-            })}
+        <div className={styles.marquee__wrapper}>
+            <div className={styles.marquee__cities}>
+                <span className={styles.marquee__cities__1}>
+                    {cities.map((city, index) => {
+                        return (
+                            <span
+                                className={`${styles.marquee__cities__city} ${city.current_city ? styles.marquee__cities__city__current : ''}`}
+                                key={index}
+                            >
+                                {city.city}/
+                            </span>
+                        )
+                    })}
+                </span>
+                <span className={styles.marquee__cities__2}>
+                    {cities.map((city, index) => {
+                        return (
+                            <span
+                                className={`${styles.marquee__cities__city} ${city.current_city ? styles.marquee__cities__city__current : ''}`}
+                                key={index}
+                            >
+                                {city.city}/
+                            </span>
+                        )
+                    })}
+                </span>
+            </div>
         </div>
     )
 }
